bb07543d1c148605e1169b2b679baef2f9fbdee2
Joseph Kottke <joe.kottke@gmail.com>
2023-03-14 10:41:34 -0600

    Adding rate-mimiting stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9.12-alpine3.15
+FROM python:3.9.12-slim-bullseye
 
 RUN mkdir /app
 WORKDIR /app
@@ -7,9 +7,12 @@ COPY src/requirements.txt /app
 RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY src/requestcounter.py /app
+COPY src/proto /app/proto
 COPY src/build-info /app/build-info
 
-RUN "md5sum /app/requestcounter.py > /app/build-info/build.signature"
+RUN python -m grpc_tools.protoc --proto_path=./proto --python_out=. --grpc_python_out=. ./proto/gubernator.proto
+RUN md5sum /app/requestcounter.py > /app/build-info/build.signature
+RUN date > /app/build-info/build.date
 
 EXPOSE 5555
 CMD [ "python3", "./requestcounter.py" ]
--- src/requestcounter.py
+++ src/requestcounter.py
@@ -1,24 +1,37 @@
 # Simple Request Counter
 
+import grpc
 import hashlib
 import os
 import pathlib
+import random
 import threading
 from quart import jsonify, Quart
 
-class AtomicCounter(object):
+import gubernator_pb2
+import gubernator_pb2_grpc
 
+class AtomicCounter(object):
     def __init__(self, initial=0):
         self.value = initial
         self._lock = threading.Lock()
-
     def increment(self, num=1):
         with self._lock:
             self.value += num
-
     def reset(self, num=0):
         with self._lock:
             self.value = num
+
+rate_limit_request = gubernator_pb2.RateLimitReq(
+    name="request_per_second",
+    unique_key="global",
+    hits=1,
+    limit=1,
+    duration=1000
+)
+
+gubernator_host = "gubernator"
+gubernator_port = 5555
 
 accepted_counter = AtomicCounter()
 denied_counter = AtomicCounter()
@@ -30,11 +43,18 @@ for filename in os.listdir('build-info'):
     buildinfo[filename] = fh.read().strip()
     fh.close()
 
+def is_permitted(key):
+    return bool(random.randint(0,1))
+
 @app.route('/')
 async def index():
-    accepted_counter.increment()
-    return {"theanswer": 42}
-
+    resp = {"theanswer": 42}
+    if is_permitted("ip:127.0.0.1"):
+        accepted_counter.increment()
+    else:
+        denied_counter.increment()
+        resp = {"error": "Too Many Requests"}
+    return resp
 
 @app.route('/metrics')
 async def metrics():
@@ -43,22 +63,23 @@ async def metrics():
         "count_denied": denied_counter.value
     }
 
-
 @app.route('/reset')
 async def reset():
     accepted_counter.reset()
     denied_counter.reset()
     return {"message": "reset complete"}
 
-
 @app.route('/build-info')
 async def build_info():
     return buildinfo
 
-if __name__ == "__main__":
+def main():
     filepath = pathlib.Path(__file__).absolute()
     contents = open(filepath).read()
     md5hash = hashlib.md5(contents.encode()).hexdigest()
     print(filepath, md5hash)
-    port = os.getenv("PORT") if os.getenv("PORT") else '5555'
+    port = os.getenv("PORT", 5555)
     app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
